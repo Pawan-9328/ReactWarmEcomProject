1b39f32c8914b76f8ac4d99ee0fe22a72381fde1
Pawan Gola <pawangola908@gmail.com>
2024-04-04 04:02:56 +0530

    All setup completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return <div className='h-screen w-screen bg-red-100'>
+
+      <nav>
+        
+
+      </nav>
+
+
+  </div>
+   
+}
+
+export default App
